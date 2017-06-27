1ee57cc3c8df76628d1928f88ab6dcc992329654
Gianni <gianniandreacavalli@gmail.com>
2017-06-27 16:37:42 +0200

    image Grid

---
+++ app/src/main/java/com/example/android/inventoryapp/GridActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.inventoryapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gacavalli on 27/06/2017.
+ */
+
+public class GridActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+    }
+}
